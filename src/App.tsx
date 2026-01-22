8bb997518e5a47277cdadb03756e3485f81aff60
[dyad] <m4littlemozarts@gmail.com>
2026-01-22 00:41:16 -0800

    [dyad] wrote 1 file(s), deleted 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -15,7 +14,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+      {/* Using Sonner for toast notifications as per project guidelines */}
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
